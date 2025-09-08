c5b226d1cb0f7c7dbba102c74518d5cf13ca81cf
Piyush Singh <singhpiyush6766@gmail.com>
2025-09-08 17:44:15 +0530

    install neon and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function page() {
   return (
     <div className='bg-red-700'>
+      <div>
+        Hello World
+      </div>
       Home page  
     </div>
   )
